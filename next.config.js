ebd01e0f3c148e81e61cda0783d9da0406bf4604
Joshua Juanzo <juanzojosh@gmail.com>
2023-07-14 10:26:19 +0800

    resolve public path in config

--- next.config.js
+++ next.config.js
@@ -1,31 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.svg/,
-      use: {
-        loader: 'svg-url-loader',
-        basePath: '/portfolio',
-      },
-    });
-    config.module.rules.push({
-      test: /\.(png|jpe?g|gif)$/i,
-      use: {
-        loader: 'file-loader',
-        options: {
-          name: '[path][name].[hash].[ext]',
-          publicPath: '/_next/static',
-          outputPath: 'static',
-          emitFile: !options.isServer,
-          basePath: '/portfolio',
-        },
-      },
-    });
-    return config;
-  },
-  output: 'export',
-  basePath: '/portfolio',
   reactStrictMode: true,
+  basePath: '/portfolio',
 };
 
 module.exports = nextConfig;
